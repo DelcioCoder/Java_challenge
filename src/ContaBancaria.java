f6c74b93c9dd5f9867df9897798f639cb4d77652
DelcioCoder <armandodelcio40@gmail.com>
2025-07-20 20:51:55 +0100

    Simulando sistema de conta bancaria em java

---
+++ src/ContaBancaria.java
@@ -0,0 +1,40 @@
+public class ContaBancaria {
+    int numeroConta;
+    double saldo;
+
+    public ContaBancaria(int numeroConta, double saldoInicial) {
+        this.numeroConta = numeroConta;
+        this.saldo = saldoInicial;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void depositar(int valor) {
+        if(valor < 0) {
+            throw new IllegalArgumentException("O valor não pode ser negativo!");
+        }
+        this.saldo += valor;
+        System.out.println("Deposito de " + valor + " feito com sucesso!");
+    }
+
+    public void sacar(int valor) {
+        if(valor > 0 && saldo >= valor) {
+            saldo -= valor;
+            System.out.println("Saque de " + valor + " feito com sucesso");
+        } else {
+            System.out.println("Saldo insuficiente!!");
+        }
+    }
+
+    public static void main(String[] args) {
+        ContaBancaria contaBancaria = new ContaBancaria(19911, 1000);
+        contaBancaria.depositar(200);
+        contaBancaria.sacar(1500);
+    }
+}
